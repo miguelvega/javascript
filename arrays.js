5e8d95d97e333a73d04a3182779109346da5752e
miguelvega <miguelvegasolde@gmail.com>
2023-11-20 17:57:23 -0500

    Comprobando resultados de arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// Expresiones
+const expresion1 = [1, 2, 3] + [4, 5, 6];
+const expresion2 = !![];
+const expresion3 = [] == true;
+const expresion4 = [10, 1, 3].sort();
+const expresion5 = [] == 0;
+
+// Mostrar resultados
+console.log(`1. [1, 2, 3] + [4, 5, 6]: ${expresion1}`);
+console.log(`2. !![]: ${expresion2}`);
+console.log(`3. [] == true: ${expresion3}`);
+console.log(`4. [10, 1, 3].sort(): ${expresion4}`);
+console.log(`5. [] == 0: ${expresion5}`);
